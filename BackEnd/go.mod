4a23c4462f728f70feafd6c2f539ff6df81cd8c2
Panakotta00 <panakotta00@gmail.com>
2022-01-15 21:33:52 +0100

    Migrate to use echo

--- BackEnd/go.mod
+++ BackEnd/go.mod
@@ -3,7 +3,8 @@ module main
 go 1.17
 
 require (
-	github.com/Masterminds/semver v1.5.0
-	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgx/v4 v4.14.1
+	github.com/labstack/echo/v4 v4.6.3
+	gorm.io/driver/postgres v1.2.3
+	gorm.io/gorm v1.22.5
 )
--- BackEnd/main.go
+++ BackEnd/main.go
@@ -2,104 +2,76 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4"
+	"github.com/labstack/echo/v4"
 	"log"
 	"main/Database"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 )
 
 var db *pgx.Conn
 
-func respondStruct(w http.ResponseWriter, status int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	err := json.NewEncoder(w).Encode(data)
+func getPackage(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "unable to dump response", http.StatusInternalServerError)
-		return
-	}
-}
-
-func getPackage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 64)
-	if err != nil {
-		http.Error(w, "invalid package-id format", http.StatusBadRequest)
-		return
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid package-id format")
 	}
 	pack, err := Database.PackageGet(db, id)
 	if err != nil {
-		http.Error(w, "package not found", http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, "package not found")
 	}
-	respondStruct(w, http.StatusOK, pack)
+	return c.JSON(http.StatusOK, pack)
 }
 
-func getPackageTags(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 64)
+func getPackageTags(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "invalide package-id format", http.StatusBadRequest)
-		return
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid package-id format")
 	}
 	tags, err := Database.PackageTags(db, id)
 	if err != nil {
-		log.Println(err)
-		http.Error(w, "package not found", http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, "package not found")
 	}
-	respondStruct(w, http.StatusOK, *tags)
+	return c.JSON(http.StatusOK, *tags)
 }
 
-func getTag(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 64)
+func getTag(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "invalid tag-id format", http.StatusBadRequest)
-		return
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid tag-id format")
 	}
 	tag, err := Database.TagGet(db, id)
 	if err != nil {
-		http.Error(w, "tag not found", http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, "tag not found")
 	}
-	respondStruct(w, http.StatusOK, tag)
+	return c.JSON(http.StatusOK, tag)
 }
 
-func getRelease(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 64)
+func getRelease(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "invalid release-id format", http.StatusBadRequest)
-		return
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid release-id format")
 	}
 	release, err := Database.ReleaseGet(db, id)
 	if err != nil {
-		http.Error(w, "release not found", http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, "release not found")
 	}
-	respondStruct(w, http.StatusOK, release)
+	return c.JSON(http.StatusOK, release)
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.ParseUint(vars["id"], 10, 64)
+func getUser(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "invalid user-id format", http.StatusBadRequest)
-		return
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid user-id format")
 	}
 	user, err := Database.UserGet(db, id)
 	if err != nil {
-		http.Error(w, "user not found", http.StatusNotFound)
-		return
+		return echo.NewHTTPError(http.StatusNotFound, "user not found")
 	}
-	respondStruct(w, http.StatusOK, user)
+	return c.JSON(http.StatusOK, user)
 }
 
 func main() {
@@ -114,20 +86,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/package/{id:[0-9]+}", getPackage)
-	r.HandleFunc("/package/{id:[0-9]+}/tags", getPackageTags)
-	r.HandleFunc("/release/{id:[0-9]+}", getRelease)
-	r.HandleFunc("/tag/{id:[0-9]+}", getTag)
-	r.HandleFunc("/user/{id:[0-9]+}", getUser)
+	e := echo.New()
 
-	srv := &http.Server{
-		Handler: r,
-		Addr:    "127.0.0.1:8000",
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
+	e.GET("/package/:id", getPackage)
+	e.GET("/package/:id/tags", getPackageTags)
+	e.GET("/release/:id", getRelease)
+	e.GET("/tag/:id", getTag)
+	e.GET("/user/:id", getUser)
 
-	log.Fatal(srv.ListenAndServe())
+	e.Logger.Fatal(e.Start(":8000"))
 }
